50dc93ddd993bd2924502e2ae9527308c6a0aa61
andy silva <andssilva.23@gmail.com>
2021-05-26 15:55:44 -0400

    feat(requeriment 1): :rocket: Test App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../RenderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +13,73 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('teste se há um menu fixo, com 3 links', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const navPoke = getByRole('navigation');
+  expect(navPoke).toBeInTheDocument();
+
+  const navPokeHome = getByRole('link', { name: /home/i });
+  expect(navPokeHome).toBeInTheDocument();
+
+  const navPokeAbout = getByRole('link', { name: /about/i });
+  expect(navPokeAbout).toBeInTheDocument();
+
+  const navPokeFavorite = getByRole('link', { name: /favorite pokémons/i });
+  expect(navPokeFavorite).toBeInTheDocument();
+});
+
+test('Ao clicar em Home é redirecionado para "/"', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  userEvent.click(getByRole('link', { name: /home/i }));
+
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/');
+});
+
+test('Ao clicar em About é redirecionado para "/about"', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  userEvent.click(getByRole('link', { name: /about/i }));
+
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/about');
+});
+
+test('Ao clicar em Favorite Pokémons é redirecionado para "/favorites"', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  userEvent.click(getByRole('link', { name: /favorite pokémons/i }));
+
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/favorites');
+});
+
+test('Ao acessar uma URL desconhecida é redirecionado para a página Not Found', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  history.push('/teste');
+
+  const pokeNotFound = getByText('Page requested not found');
+  expect(pokeNotFound).toBeInTheDocument();
 });
